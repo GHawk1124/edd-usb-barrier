2f517bee01feb6a03d6f213c053f6cbffd0ebb73
Garrett Comes <garrett1124@icloud.com>
2022-01-06 12:03:02 -0700

    fixed cmake file

--- src/unix/main.cpp
+++ src/unix/main.cpp
@@ -8,58 +8,41 @@
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
 
-// Main code
 int main(int, char **) {
-  // Setup SDL
-  // (Some versions of SDL before <2.0.10 appears to have performance/stalling
-  // issues on a minority of Windows systems, depending on whether
-  // SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version
-  // of SDL is recommended!)
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) !=
       0) {
     printf("Error: %s\n", SDL_GetError());
     return -1;
   }
 
-  // Setup window
   SDL_WindowFlags window_flags =
       (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-  SDL_Window *window = SDL_CreateWindow(
-      "Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+  SDL_Window *window =
+      SDL_CreateWindow("USB Barrier", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 
-  // Setup SDL_Renderer instance
   SDL_Renderer *renderer = SDL_CreateRenderer(
       window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
   if (renderer == NULL) {
     SDL_Log("Error creating SDL_Renderer!");
     return -1;
   }
-  // SDL_RendererInfo info;
-  // SDL_GetRendererInfo(renderer, &info);
-  // SDL_Log("Current SDL_Renderer: %s", info.name);
 
-  // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO &io = ImGui::GetIO();
   (void)io;
   // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable
   // Keyboard Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; //
-  // Enable Gamepad Controls
 
-  // Setup Dear ImGui style
-  // ImGui::StyleColorsDark();
-  ImGui::StyleColorsClassic();
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsClassic();
 
-  // Setup Platform/Renderer backends
+  // SetupStyleFromHue();
+
   ImGui_ImplSDL2_InitForSDLRenderer(window);
   ImGui_ImplSDLRenderer_Init(renderer);
 
-  // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can
-  // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
-  // them.
   // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
   // need to select the font among multiple.
   // - If the file cannot be loaded, the function will return NULL. Please
@@ -80,67 +63,37 @@ int main(int, char **) {
   // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
   // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
-  // Our state
-  bool show_demo_window = false;
   bool show_another_window = false;
   ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-  // Main loop
   bool done = false;
   while (!done) {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
-    // tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
-    // your main application.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
-    // data to your main application. Generally you may always pass all inputs
-    // to dear imgui, and hide them from your application based on those two
-    // flags.
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       ImGui_ImplSDL2_ProcessEvent(&event);
-      if (event.type == SDL_QUIT)
+      if (event.type == SDL_QUIT) {
         done = true;
+      }
       if (event.type == SDL_WINDOWEVENT &&
           event.window.event == SDL_WINDOWEVENT_CLOSE &&
-          event.window.windowID == SDL_GetWindowID(window))
+          event.window.windowID == SDL_GetWindowID(window)) {
         done = true;
+      }
     }
 
-    // Start the Dear ImGui frame
     ImGui_ImplSDLRenderer_NewFrame();
     ImGui_ImplSDL2_NewFrame(window);
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window)
-      ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-    // to created a named window.
+    // GUI Code Here
     {
       static float f = 0.0f;
       static int counter = 0;
-
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                     // and append into it.
-
-      ImGui::Text("This is some useful text."); // Display some text (you can
-                                                // use a format strings too)
-      ImGui::Checkbox(
-          "Demo Window",
-          &show_demo_window); // Edit bools storing our window open/close state
+      ImGui::Begin("Hello, world!");
+      ImGui::Text("This is some useful text.");
       ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float", &f, 0.0f,
-                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3(
-          "clear color",
-          (float *)&clear_color); // Edit 3 floats representing a color
-
+      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+      ImGui::ColorEdit3("clear color", (float *)&clear_color);
       if (ImGui::Button("Button")) {
         counter++;
       }
@@ -152,13 +105,8 @@ int main(int, char **) {
       ImGui::End();
     }
 
-    // 3. Show another simple window.
     if (show_another_window) {
-      ImGui::Begin(
-          "Another Window",
-          &show_another_window); // Pass a pointer to our bool variable (the
-                                 // window will have a closing button that will
-                                 // clear the bool when clicked)
+      ImGui::Begin("Another Window", &show_another_window);
       ImGui::Text("Hello from another window!");
       if (ImGui::Button("Close Me")) {
         show_another_window = false;
@@ -166,17 +114,16 @@ int main(int, char **) {
       ImGui::End();
     }
 
-    // Rendering
     ImGui::Render();
-    SDL_SetRenderDrawColor(
-        renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255),
-        (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+    SDL_SetRenderDrawColor(renderer, (uint8_t)(clear_color.x * 255),
+                           (uint8_t)(clear_color.y * 255),
+                           (uint8_t)(clear_color.z * 255),
+                           (uint8_t)(clear_color.w * 255));
     SDL_RenderClear(renderer);
     ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
     SDL_RenderPresent(renderer);
   }
 
-  // Cleanup
   ImGui_ImplSDLRenderer_Shutdown();
   ImGui_ImplSDL2_Shutdown();
   ImGui::DestroyContext();
