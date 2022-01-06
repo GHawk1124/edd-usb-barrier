9cda882d605fefcecc13a53892b2cc0eebeaa5b2
Garrett Comes <garrett1124@icloud.com>
2022-01-06 13:04:10 -0700

    added cross-compilation from mac to windows

--- ci/macOS.sh
+++ ci/macOS.sh
@@ -1,6 +1,14 @@
 git pull
 mkdir -p build
-cd build
-cmake ..
+cd build || exit
+if [ "$1" == "darwin" ]; then
+  cmake ..
+elif [ "$1" == "windows" ]; then
+ cmake -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+      -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .. 
+else
+  echo "No target provided. Use \"darwin\" or \"windows\""
+  exit 1
+fi
 make
-cd ..
+cd .. || exit
